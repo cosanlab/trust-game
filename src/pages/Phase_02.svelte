8d66ff421a8d392e01937be6312640d0c29372fa
wasita <wasita.mahaphanit.gr@dartmouth.edu>
2023-01-19 23:25:30 -0500

    feat: phase-02 Qs

--- src/pages/Phase_02.svelte
+++ src/pages/Phase_02.svelte
@@ -7,36 +7,123 @@ Data needed:
 - Multiplier -> globals in utils.js
 
 Data stored/modified:
-- Investor 1st order
-- Trustee 2nd order
+- Investor 1st order -- "i wonder how much trustee will return to me"
+- Trustee 2nd order -- "i wonder how much investor expects me to return to them"
 -->
 <script>
   import { createEventDispatcher } from "svelte";
-  import { userStore, groupStore, roundHalf } from "../utils.js";
+  import {
+    userStore,
+    groupStore,
+    saveQData,
+    round2,
+    globalVars,
+  } from "../utils.js";
   import Loading from "../components/Loading.svelte";
+  import PainScale from "../components/PainScale.svelte";
+  import Rating from "../components/Rating.svelte";
   import Button from "../components/Button.svelte";
 
-  let site = $groupStore.trials[$groupStore.currentTrial].site;
-  let protocol = roundHalf(
-    $groupStore.trials[$groupStore.currentTrial].pain_dur
-  );
+  console.log("userStore", $userStore);
+  console.log("groupStore", $groupStore);
+  console.log("globalVars", globalVars);
 
   const dispatch = createEventDispatcher();
+  let submitted = false;
+  let disableInput = false;
+  let currentQ = 0;
+  let showButton = true;
+  let switchToRatingScale = false;
+  let questions;
 
-  function goToDelivery() {
-    dispatch("to-phase-03");
+  // GET TRIAL DATA
+  // Shared endowment
+  let endowment = $groupStore.trials[$groupStore.currentTrial].endowment;
+
+  const otherName =
+    $userStore.role === "investor" ? $groupStore.T_name : $groupStore.I_name;
+
+  // Now setup rating scales
+  let i_r = 0.5 * endowment; // investor's rating
+  let t_r = 0.5 * endowment; // trustee's rating
+
+  if ($userStore.role == "investor") {
+    questions = [
+      {
+        questionText: `How much do you think ${$groupStore.T_name} will return to you?`,
+        rating: i_r,
+        questionType: "other",
+        endowment: endowment,
+      },
+    ];
+  } else {
+    // For Trustee we ask how much they expect the investor to share with them
+    questions = [
+      {
+        questionText: `How much do you think ${$groupStore.I_name} expects you to return to them?`,
+        rating: t_r,
+        questionType: "self",
+        endowment: endowment,
+      },
+    ];
+  }
+
+  console.log("questions", questions);
+
+  async function goto_phase_02() {
+    submitted = true;
+    await saveQData(questions);
+    dispatch("to-phase-02");
+  }
+
+  async function getNextQuestion() {
+    console.log("currentQ", currentQ);
+    // If they're done answering move to next state
+    if (currentQ === questions.length - 1) {
+      await goto_phase_02();
+    } else {
+      currentQ = currentQ + 1;
+      showButton = true;
+      switchToRatingScale = false;
+      disableInput = false;
+    }
   }
 </script>
 
-<div class="flex h-screen text-center">
-  <div class="m-auto">
-    {#if $userStore.role === "receiver"}
-      <h1 class="mb-4 text-2xl">
-        Experimenter please place the thermode on site {site} and use protocol {protocol}
-      </h1>
-      <Button on:click={goToDelivery}>Ready</Button>
-    {:else}
-      <h1 class="mb-4 text-2xl">Waiting for experimenter to place thermode</h1>
-    {/if}
+{#if submitted}
+  <Loading text={"Waiting for other participants..."} />
+{:else}
+  <div class="w-3/5 mx-auto">
+    <div class="min-w-full pb-32 text-center">
+      <div class="my-10">
+        {#if switchToRatingScale}
+          <Rating
+            questionText={questions[currentQ].questionText}
+            bind:rating={questions[currentQ].rating}
+          />
+        {:else if $userStore.role === "investor"}
+          <PainScale
+            bind:rating={questions[currentQ].rating}
+            questionText={questions[currentQ].questionText}
+            endowment={questions[currentQ].endowment}
+            questionType={questions[currentQ].questionType}
+            disabled={disableInput}
+          />
+          <hr class="w-full my-8 border-black border-dashed" />
+          <hr class="w-full my-4 border-white" />
+        {:else if $userStore.role === "trustee"}
+          <PainScale
+            bind:rating={questions[currentQ].rating}
+            questionText={questions[currentQ].questionText}
+            endowment={questions[currentQ].endowment}
+            questionType={questions[currentQ].questionType}
+            disabled={disableInput}
+          />
+        {/if}
+        {#if showButton}
+          <Button on:click={getNextQuestion}>Next</Button>
+        {/if}
+      </div>
+    </div>
   </div>
-</div>
+{/if}
