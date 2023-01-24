cdd907bdf10c0f449c4eafd0957089ca7b2efb9a
wasita <wasita.mahaphanit.gr@dartmouth.edu>
2023-01-24 18:56:08 -0500

    feat: color-coding of outcome explanation text

--- src/pages/Phase_04.svelte
+++ src/pages/Phase_04.svelte
@@ -44,42 +44,65 @@
 
   // Shared endowment
   let endowment = $groupStore.trials[currentTrial].endowment;
+
   let I_expectation = $groupStore.trials[currentTrial].I_1ST_ORDER_EXPECTATION;
   let T_prediction = $groupStore.trials[currentTrial].T_PREDICTION;
   let I_earnings = $groupStore.trials[currentTrial].I_EARNED;
   let T_earnings = $groupStore.trials[currentTrial].T_EARNED;
   let I_received = $groupStore.trials[currentTrial].T_CHOICE;
   let T_received = $groupStore.trials[currentTrial].I_CHOICE;
+  let trusteeReceivedMultiplied = T_received * globalVars.multiplier;
 
   const earnings = round2(
     $userStore.role === "investor" ? I_earnings : T_earnings
   );
-  const expectFromOther =
+  const expectedAmountFromOther =
     $userStore.role === "investor" ? I_expectation : T_prediction;
   const receivedAmount =
     $userStore.role === "investor" ? I_received : T_received;
   const keptAmount = round2(
     $userStore.role === "investor" ? endowment - T_received : T_received
   );
-  // TODO: color-code based on info to enable quicker reading of multiple lines
-  // e.g. started is one color, expected is another
-  // maybe color-code names too
+
   const otherName =
     $userStore.role === "investor" ? $groupStore.T_name : $groupStore.I_name;
 
+  const whoText = $userStore.role === "investor" ? "They" : "You";
+
   const outcomeExplanation =
     $userStore.role === "investor"
-      ? `You started with $${endowment} and gave $${T_received} to ${otherName}, keeping $${keptAmount} for yourself.
-        With the multiplier, ${otherName} then had $${
-          T_received * globalVars.multiplier
-        }. You expected $${expectFromOther} but received $${receivedAmount} from ${otherName},
-        resulting in a total earnings of $${earnings}
-        for this trial.`
-      : `${otherName} started with $${endowment}. You expected $${expectFromOther} but received $${receivedAmount} from ${otherName}.
-        With the multiplier, you then had $${
-          receivedAmount * globalVars.multiplier
-        }. ${otherName} expected $${I_expectation} from you but you gave $${I_received} to ${otherName},
-        resulting in a total earnings of $${earnings} for this trial.`;
+      ? {
+          otherName: "You",
+          started: "started",
+          withEndowment: `with $${endowment}.`,
+          whoExpected1: `${otherName}`,
+          expected: "expected",
+          youExpectAmount: `$${T_prediction}`,
+          youActualReceived: `but received`,
+          youActualReceivedAmount: `$${T_received}`,
+          youActualReceivedFrom: `from you.`,
+          withMultiplier: `With the multiplier, ${whoText.toLowerCase()} then had `,
+          trusteeReceived: `$${trusteeReceivedMultiplied}.`,
+          whoExpected2: "You",
+          investorExpected: `$${I_expectation} from ${otherName} but they gave you $${I_received},`,
+          trialResults: `resulting in a total earnings of $${earnings} for this trial.`,
+        }
+      : {
+          otherName: otherName,
+          started: "started",
+          withEndowment: `with $${endowment}.`,
+          whoExpected1: whoText,
+          expected: "expected",
+          youExpectAmount: `$${T_prediction}`,
+          youActualReceived: `but received`,
+          youActualReceivedAmount: `$${T_received}`,
+          youActualReceivedFrom: `from ${otherName}.`,
+          withMultiplier: `With the multiplier, ${whoText.toLowerCase()} then had `,
+          trusteeReceived: `$${trusteeReceivedMultiplied}.`,
+          whoExpected2: "They",
+          investorExpected: `$${I_expectation} from you but you gave them $${I_received},`,
+          trialResults: `resulting in a total earnings of $${earnings} for this trial.`,
+        };
 
   let questions;
   // Initialize all scales to their mid-point assumpting they're 100pt scales
@@ -106,17 +129,17 @@
         rating: r2,
       },
       {
-        questionText: `How guilty do you think ${otherName} feels?`,
+        questionText: `How guilty do you think ${outcomeExplanation.otherName} feels?`,
         type: "other_guilt",
         rating: r3,
       },
       {
-        questionText: `How angry do you think ${otherName} feels?`,
+        questionText: `How angry do you think ${outcomeExplanation.otherName} feels?`,
         type: "other_angry",
         rating: r4,
       },
       {
-        questionText: `How close do you feel to ${otherName}?`,
+        questionText: `How close do you feel to ${outcomeExplanation.otherName}?`,
         type: "close",
         rating: r5,
       },
@@ -141,19 +164,37 @@
 {:else}
   <div class="w-1/2 mx-auto">
     <div class="min-w-full pb-32 text-center">
+      <!-- <p class="mb-1 text-4xl">
+        You earned <span class:text-green-500="{role == 
+      'investor'}" class:text-red-500="{}">${earnings}</span> for this trial!
+      </p> -->
       <p class="mb-1 text-4xl">
-        You earned ${earnings} for this trial!
+        You earned <span class="text-green-500">${earnings}</span> for this trial!
       </p>
       <p class="mb-1 text-lg">
-        {outcomeExplanation}
+        {outcomeExplanation.otherName}
+        <span class="text-green-500">{outcomeExplanation.started}</span>
+        {outcomeExplanation.withEndowment}
+        {outcomeExplanation.whoExpected1}
+        <span class="text-purple-500">{outcomeExplanation.expected}</span>
+        {outcomeExplanation.youExpectAmount}
+        {outcomeExplanation.youActualReceived}
+        {outcomeExplanation.youActualReceivedAmount}
+        {outcomeExplanation.youActualReceivedFrom}
+        {outcomeExplanation.withMultiplier}
+        {outcomeExplanation.trusteeReceived}
+        {outcomeExplanation.whoExpected2}
+        <span class="text-purple-500">{outcomeExplanation.expected}</span>
+        {outcomeExplanation.investorExpected}
+        {outcomeExplanation.trialResults}
       </p>
-
+      <!-- Svelte HTML evaluator -->
+      <!-- @html{eval} -->
       {#each questions as question, i}
         {#if i === 2 || i == 6}
           <hr class="w-full my-16 border-black border-dashed" />
         {/if}
         {#if question.questionText.includes("compensate")}
-          <!-- TODO: swap MouseRating with standard Rating -->
           <div class="my-8">
             <Rating
               questionText={question.questionText}
@@ -166,7 +207,6 @@
             />
           </div>
         {:else}
-          <!-- TODO: swap MouseRating with standard Rating -->
           <div class="my-8">
             <Rating
               questionText={question.questionText}
